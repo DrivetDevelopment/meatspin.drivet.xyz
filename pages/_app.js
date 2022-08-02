3d36267a691998cfbfcb1a51e721c53503efc89b
Slayer <aksuplayz1337@gmail.com>
2022-08-02 16:28:11 +0300

    disable user events

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <title>Meatspin</title>
+
       <Component {...pageProps} />
       <footer style={{ position: 'absolute', bottom: '10px' }}>this is made as a joke, please dont take it seriously :D</footer>
     </>
